330a759ee34773580e0b7436e82ebc849da77c51
jihoopark <wlgn8648@gmail.com>
2023-05-26 22:29:02 +0900

    파일 정리

--- home/config2.py
+++ home/config2.py
@@ -1,14 +1,17 @@
-import gensim.downloader as api
-import numpy as np
 import random
-import networkx as nx
+
+import gensim.downloader as api
 import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
 # git
 # Load pre-trained Word2Vec model
 model = api.load('word2vec-google-news-300')
 
+
 def most_similar_bandit(word, num_options=5, epsilon=0.1):
-    similar_words = model.most_similar(positive=[word], topn=num_options*10)
+    similar_words = model.most_similar(positive=[word], topn=num_options * 10)
     options = random.sample(similar_words, num_options)
     rewards = [0] * num_options
     counts = [0] * num_options
@@ -25,11 +28,15 @@ def most_similar_bandit(word, num_options=5, epsilon=0.1):
 
     return [options[i] for i in np.argsort(rewards)[::-1]]
 
+
 def draw_mind_map(graph):
     plt.figure(figsize=(16, 10))
     pos = nx.spring_layout(graph, seed=42)
-    nx.draw(graph, pos, with_labels=True, node_color="skyblue", font_size=14, font_weight="bold", node_size=3000, alpha=0.8)
-    nx.draw_networkx_edge_labels(graph, pos, edge_labels={(n1, n2): f"{w:.2f}" for n1, n2, w in graph.edges(data="weight")}, font_size=12)
+    nx.draw(graph, pos, with_labels=True, node_color="skyblue", font_size=14, font_weight="bold", node_size=3000,
+            alpha=0.8)
+    nx.draw_networkx_edge_labels(graph, pos,
+                                 edge_labels={(n1, n2): f"{w:.2f}" for n1, n2, w in graph.edges(data="weight")},
+                                 font_size=12)
     plt.title("Mind Map", fontsize=24)
     plt.axis("off")
     plt.show()
@@ -46,6 +53,7 @@ def add_graph(graph, current_word):
         if not graph.has_edge(current_word, word):
             graph.add_edge(current_word, word, weight=suggestions[i][1])
     return suggestions
+
 
 def mind_mapping():
     center_word = input("Enter the center word for brainstorming: ")
