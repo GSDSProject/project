e8f803b648683b1ac390f00be329a8479e35a32a
gyuyeolK <gyuyeolkim@snu.ac.kr>
2023-05-26 15:18:33 +0900

    config2.py

--- home/config2.py
+++ home/config2.py
@@ -34,39 +34,38 @@ def draw_mind_map(graph):
     plt.axis("off")
     plt.show()
 
+
+def add_graph(graph, current_word):
+    print(f"\nSuggestions for '{current_word}':")
+    suggestions = most_similar_bandit(current_word)
+
+    for i, (word, _) in enumerate(suggestions):
+        print(f"{i + 1}. {word}")
+        if not graph.has_node(word):
+            graph.add_node(word)
+        if not graph.has_edge(current_word, word):
+            graph.add_edge(current_word, word, weight=suggestions[i][1])
+    return suggestions
+
 def mind_mapping():
     center_word = input("Enter the center word for brainstorming: ")
     print("Enter 'exit' to finish the mind mapping process.")
     print("Enter 'nothing' if you want to see more suggestions.")
-    current_word = center_word
 
+    current_word = center_word
     graph = nx.Graph()
     graph.add_node(center_word)
 
     while True:
-        print(f"\nSuggestions for '{current_word}':")
-        suggestions = most_similar_bandit(current_word)
-
-        for i, (word, _) in enumerate(suggestions):
-            print(f"{i + 1}. {word}")
-
-            if not graph.has_node(word):
-                graph.add_node(word)
-
-            if not graph.has_edge(current_word, word):
-                graph.add_edge(current_word, word, weight=suggestions[i][1])
-
+        suggestions = add_graph(graph, current_word)
         choice = input("Choose a word by entering its number or input 'nothing' or 'exit': ").strip()
-
         if choice.lower() == "exit":
             break
         elif choice.lower() == "nothing":
             continue
-
         try:
             index = int(choice) - 1
             current_word = suggestions[index][0]
         except (ValueError, IndexError):
             print("Invalid input. Please try again.")
-
     return graph
